fab235b87aea784083b5a46d408a24242c789810
myalgoritma <algoritma@post.com>
2016-03-29 17:29:02 -0300

    usuarios 4.0.2

--- modules/users/client/controllers/settings/edit-profile.client.controller.js
+++ modules/users/client/controllers/settings/edit-profile.client.controller.js
@@ -1,22 +1,29 @@
-(function () {
-  'use strict';
+'use strict';
 
-  angular
-    .module('users')
-    .controller('EditProfileController', EditProfileController);
-
-  EditProfileController.$inject = ['$scope', '$http', '$location', 'Users', 'Authentication'];
-
-  function EditProfileController($scope, $http, $location, Users, Authentication) {
-    var vm = this;
-
-    vm.user = Authentication.user;
-    vm.updateUserProfile = updateUserProfile;
-    console.log("TESTE");
+angular.module('users').controller('EditProfileController', ['$scope', '$http', '$location', 'Users', 'Authentication',
+  function ($scope, $http, $location, Users, Authentication) {
+    $scope.user = Authentication.user;
 
     // Update a user profile
-    function updateUserProfile(isValid) {
-      
-    }
+    $scope.updateUserProfile = function (isValid) {
+      $scope.success = $scope.error = null;
+
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'userForm');
+
+        return false;
+      }
+
+      var user = new Users($scope.user);
+
+      user.$update(function (response) {
+        $scope.$broadcast('show-errors-reset', 'userForm');
+
+        $scope.success = true;
+        Authentication.user = response;
+      }, function (response) {
+        $scope.error = response.data.message;
+      });
+    };
   }
-}());
+]);
